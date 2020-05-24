d8a521d6802dbc9d08f472d4a24c6647a16523f6
breadone <pradyun.setti@gmail.com>
2020-05-25 09:29:20 +1200

    initial

---
+++ src/stacksonstacksonstacks.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class stacksonstacksonstacks {
+    public static void main(String[] args) throws Exception {
+        stacksonstacksonstacksonstacks();
+        stacksonstacksonstacks stacks = new stacksonstacksonstacks();
+    }
+
+    public static void stacksonstacksonstacksonstacks() {
+        System.out.println("stacks are fucking stupid ngl");
+        Stack<String>frick = new Stack<>();
+
+         
+        int ok[] = new int[20];
+
+        frick.push("gamer)");
+
+        System.out.println(frick);
+
+
+
+    }
+
+
+}
+
